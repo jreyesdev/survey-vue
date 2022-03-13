0fa15166bf6593987d6c2c31405f9a0552e1ded6
Jesus Reyes <djrm1204@gmail.com>
2022-03-13 19:27:37 -0400

    Proteccion de rutas

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,12 +4,14 @@ import Layout from '../components/layouts/Layout.vue';
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
 import Surveys from '../views/Surveys.vue';
+import { store } from '../store';
 
 const routes: RouteRecordRaw[] = [
   {
     name: 'Layout',
     path: '/',
     redirect: '/dashboard',
+    meta: { requiresAuth: true },
     component: Layout,
     children: [
       { name: 'Dashboard', path: '/dashboard', component: Dashboard },
@@ -28,7 +30,22 @@ const routes: RouteRecordRaw[] = [
   },
 ];
 
-export default createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !store.state.user.token) {
+    next({ name: 'Login' });
+  } else if (
+    store.state.user.token &&
+    (to.name === 'Login' || to.name === 'Register')
+  ) {
+    next({ name: 'Dashboard' });
+  } else {
+    next();
+  }
+});
+
+export default router;
